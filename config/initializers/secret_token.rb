daeb5e2dabc432c85556bddf6f04df9e446b1a41
Helen Hood <helen.c.hood@gmail.com>
2013-12-30 20:08:17 -0500

    Initial commit - new Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FoodInventory::Application.config.secret_key_base = '3033dfae8ae2ff84659f411300944d236dbaff34c837e7783d2bbff5c1d9e2783138060b6b3a77f6bffef547f321b6acc9d14373797e1465d30e0732dbd8c0f7'
